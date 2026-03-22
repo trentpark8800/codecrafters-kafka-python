69fcaaac83c73b2daa4a21586151d2e7a9d1945c
trentpark8800 <47793696+trentpark8800@users.noreply.github.com>
2026-03-22 08:43:23 +0000

    Add correlation id and message size response to server

--- app/main.py
+++ app/main.py
@@ -7,7 +7,20 @@ def main():
     print("Logs from your program will appear here!")
 
     server = socket.create_server(("localhost", 9092), reuse_port=True)
-    server.accept() # wait for client
+    connection, address = server.accept()
+
+    print(f"New client connected from {address}.")
+
+    with connection:
+        while True:
+            data = connection.recv(1024)
+            if not data:
+                break
+            message_size = len(data).to_bytes(4, "big")
+            correlation_id = int(7).to_bytes(4, "big")
+            connection.sendall(message_size)
+            connection.sendall(correlation_id)
+
 
 
 if __name__ == "__main__":
