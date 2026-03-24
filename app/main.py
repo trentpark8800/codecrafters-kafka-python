9584398ce54ad91bf18d309d0a78af34a546f79b
trentpark8800 <47793696+trentpark8800@users.noreply.github.com>
2026-03-24 07:21:33 +0000

    Change offset of bytes for correlation id

--- app/main.py
+++ app/main.py
@@ -17,7 +17,7 @@ def main():
             if not data:
                 break
             message_size = len(data).to_bytes(4, "big")
-            correlation_id = data[4:8]
+            correlation_id = data[8:12]
             connection.sendall(message_size)
             connection.sendall(correlation_id)
 
