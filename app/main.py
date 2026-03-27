8a0a30e8a8b1e7876a3e2f637bda753c28b40035
trentpark8800 <47793696+trentpark8800@users.noreply.github.com>
2026-03-27 13:43:47 +0000

    Add message parsing and api error

--- app/main.py
+++ app/main.py
@@ -1,4 +1,64 @@
-import socket  # noqa: F401
+import socket
+from dataclasses import dataclass
+from typing import List
+
+
+@dataclass
+class Message:
+    correlation_id: int
+    request_api_version: int
+    request_api_key: int
+    message_size: int
+
+
+@dataclass
+class Response:
+    correlation_id: int
+    error_code: int
+
+
+def read_message(data: bytes) -> Message:
+
+    message: Message = Message(
+        correlation_id=int(data[8:12]),
+        message_size=int(data[0:4]),
+        request_api_key=int(data[4:6]),
+        request_api_version=int(data[6:8]),
+    )
+
+    return message
+
+
+def parse_message(message: Message) -> Response:
+
+    supported_versions: List[int] = [4]
+
+    error_code: int = 0
+
+    if message.request_api_version not in supported_versions:
+        print(f"Unsupported version {Message.request_api_version}.")
+        error_code = 35
+    
+    response: Response = Response(
+        correlation_id=message.correlation_id,
+        error_code=error_code,
+    )
+
+    return response
+
+
+def parse_response(response: Response) -> bytes:
+
+    response_data: bytes = response.correlation_id.to_bytes(4, "big") + response.error_code.to_bytes(2, "big")
+
+    response_size = len(response_data).to_bytes(4, "big")
+
+    return response_size + response_data
+
+
+def send_response(connection: socket.SocketType, data: bytes) -> None:
+
+    connection.sendall(data)
 
 
 def main():
@@ -16,11 +76,10 @@ def main():
             data = connection.recv(1024)
             if not data:
                 break
-            message_size = len(data).to_bytes(4, "big")
-            correlation_id = data[8:12]
-            connection.sendall(message_size)
-            connection.sendall(correlation_id)
-
+            message: Message = read_message(data)
+            response: Response = parse_message(message)
+            response_bytes: bytes = parse_response(response)
+            send_response(connection, response_bytes)
 
 
 if __name__ == "__main__":
